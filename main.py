a3ab33fe8f01eaba53b666984d46dc60b386b78c
Kamil  Goś <kamil.gos00@gmail.com>
2021-01-16 12:23:53 +0100

    Fixed problem with window exit

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import logging
 import time
 from random import randint
 from window import Algorithm
+from PyQt5.QtWidgets import QApplication, QWidget
+import sys
+
 LOG_FORMAT = '%(levelname)-10s %(name)-20s %(funcName)-20s  %(message)s'
 logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 LOG = logging.getLogger(__name__)
@@ -24,5 +27,5 @@ PUCKS_NUM = 30
 
 
 if __name__ == "__main__":
-
+    app = QApplication(sys.argv)
     al = Algorithm()
